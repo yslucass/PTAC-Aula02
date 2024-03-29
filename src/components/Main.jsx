62682fffefbc01bd7fc4714b562c07be8d5dab09
yslucass <161073831+yslucass@users.noreply.github.com>
2024-03-29 01:20:43 +0000

    atividade 22/03

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,29 @@ import { useState } from "react";
 export default function Main(){
     const [nome, setNome] = useState("");
     const [telefone, setTelefone] = useState("")
+    const [listaContatos, setListaContatos] = useState([])
+    const [cpf, setCpf] = useState("")
+
+   const registrar = (event) =>{
+     setListaContatos([...listaContatos, {nome, telefone, cpf}])
+     
+   };
+  
     return (
         <main>
-            <form>
+            <form action="" onSubmit={registrar} >
+                <label htmlFor="nome">Nome:</label>
                 <input type="text" name="" id="" onChange={(event)=> setNome(event.target.value)}/>
                 {nome}
-
+          
+                <label htmlFor="telefone">Telefone:</label>
                 <input type="tel" name="" id="" onChange={(event)=> setTelefone(event.target.value)}/>
                 {telefone}
+
+                <label htmlFor="cpf">Cpf:</label>
+                <input type="text" name="" id="" onChange={(event)=> setCpf(event.target.value)}
+                  {cpf}
+          <button>Salvar</button>
             </form>
         </main>
     )
